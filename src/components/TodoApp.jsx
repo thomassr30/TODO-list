aba06ec25331da3a06af1e3ca6714d9239c1fa7d
thomassr30 <thomassoto94@gmail.com>
2022-06-02 22:30:00 -0400

    add importación de componente Todo

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo'
 
 const TodoApp = () => {
 
@@ -47,23 +48,7 @@ const TodoApp = () => {
                         
                         {
                             todos.map(todo => (
-                                <div key={todo.id} className="card my-4">
-                                    <div className="card-body">
-                                        <div className="row">
-                                            <div className="col-8">
-                                                {todo.title}
-                                            </div>
-                                            <div className="col-4">
-                                                <button className='btn btn-warning me-3 rounded-pill'>
-                                                    Editar
-                                                </button>
-                                                <button className='btn btn-danger rounded-pill'>
-                                                    Eliminar
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                               <Todo key={todo.id} todo={todo}/>
                             )
                             )
                         }
